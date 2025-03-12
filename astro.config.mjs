15154e10aef89f7825e94de17ed5ee79259082c7
Meyer <meyerlouka6@gmail.com>
2025-03-12 13:01:29 +0000

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
+import svelteIntegration from '@astrojs/svelte';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+	svelteIntegration()
+  ]
+});
---
+++ src/components/Quiz.svelte
@@ -0,0 +1,62 @@
+<script>
+    export let questions = [];
+  
+    let correct = 0;
+    let numAnswered = 0;
+    const MAX_QUESTIONS = questions.length;
+    let showFeedback = false;
+  
+    function handleAnswer(userSaysMalicious) {
+        if (numAnswered < MAX_QUESTIONS) {
+            if (userSaysMalicious === questions[numAnswered].malicious) {
+                correct++;
+            }
+            numAnswered++;
+            showFeedback = true;
+        }
+    }
+  
+    function next() {
+        if (numAnswered >= MAX_QUESTIONS) {
+			localStorage.setItem('correct', correct)
+            window.location.href = "/dashboard";
+        } else {
+            showFeedback = false;
+        }
+    }
+</script>
+
+<div class="score-display">
+    <p>Score: {correct} / {numAnswered}</p>
+</div>
+
+<div id="email-container">
+    <p>Sample email content goes here.</p>
+</div>
+
+{#if showFeedback}
+    <div class="feedback">
+        <p>{questions[numAnswered - 1].feedback}</p>
+        <button 
+            on:click={next}
+            class="btn {numAnswered >= MAX_QUESTIONS ? 'btn-success' : 'btn-secondary'} mt-2">
+            {numAnswered >= MAX_QUESTIONS ? 'Go to Dashboard' : 'Next'}
+        </button>
+    </div>
+{:else}
+    <div class="button-container">
+        <button on:click={() => handleAnswer(false)} class="btn btn-primary">Safe</button>
+        <button on:click={() => handleAnswer(true)} class="btn btn-danger">Not Safe</button>
+    </div>
+{/if}
+
+<style>
+    #email-container,
+    .button-container,
+    .feedback,
+    .score-display {
+        max-width: 700px;
+        margin: 20px auto;
+        text-align: center;
+    }
+</style>
